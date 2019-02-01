4590b4b144e02cd22a30348b29963e48ba5fa1dc
joohansson <d84@outlook.com>
2019-02-01 08:29:49 +0100

    Search by category

---
+++ src/js/links.js
@@ -0,0 +1,41 @@
+/** Find all links and make them searchable */
+$(function () {
+	var links = [];
+	$('.link-content > ul').find('a[href]').each(function(){
+		//links.push({value: $(this).text(), data: {category: $(this).closest('.link-content').children('h2:first').text(), url: this.href}});
+		links.push({value: $(this).text(), data: {category: $(this).closest('ul').prevAll('h3:first').text(), url: this.href}});
+		//console.log($(this).text());
+		//console.log(this.href);
+	});
+
+    // Initialize search autocomplete:
+    $('#autocomplete-dynamic').autocomplete({
+        lookup: links,
+		onSelect: function (suggestion) {
+			//window.location.href = suggestion.data.url; //Go to link when selecting the link
+			$('#go-btn').attr('href', suggestion.data.url); //set link button
+
+			//Enable button
+			if ($(this).val() != "") {
+				$('#go-btn').css("pointer-events", "auto");
+			}
+		},
+		groupBy: 'category',
+		lookupFilter: function (suggestion, query, queryLowerCase) {
+            return suggestion.value.toLowerCase().indexOf(queryLowerCase) === 0 || suggestion.data.category.toLowerCase().indexOf(queryLowerCase) === 0; //checking with both label and category
+        }
+    });
+	
+	$('#autocomplete-dynamic').on("change paste keyup input", function() {
+		if ($(this).val() == "") {
+			$('#go-btn').attr('href', '/'); //reset link button
+			$('#go-btn').css("pointer-events", "none");
+		}
+	});
+	
+	//Disable link button by default
+	if ($(this).val() == "") {
+		$('#go-btn').attr('href', '/'); //reset link button
+		$('#go-btn').css("pointer-events", "none");
+	}
+});
