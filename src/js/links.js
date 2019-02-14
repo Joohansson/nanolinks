227befb5ad42556769abd2ba55f2c141c1854a5e
Joohansson <d84@outlook.com>
2019-02-14 08:28:10 +0100

    Improved search

--- src/js/links.js
+++ src/js/links.js
@@ -1,10 +1,19 @@
-/** Find all links and make them searchable */
+/** Find all links and make them searchable
+https://www.devbridge.com/sourcery/components/jquery-autocomplete
+*/
+
 $(function () {
     function resetButton() {
         $('#go-btn').attr('href', '/'); //reset link button
         $('#go-btn').css("pointer-events", "none");
     };
-    
+
+    // Set search field to focus if not incoming request is a in-page link
+    if (location.hash == "") {
+        $('#autocomplete-dynamic').focus();
+    }
+
+    // Load all links into array
     var links = [];
     $('.link-content > ul').find('a[href]').each(function(){
         //links.push({value: $(this).text(), data: {category: $(this).closest('.link-content').children('h2:first').text(), url: this.href}});
@@ -25,23 +34,40 @@ $(function () {
                 $('#go-btn').css("pointer-events", "auto");
             }
         },
+        showNoSuggestionNotice: true,
+        noSuggestionNotice: "No link or category found",
         groupBy: 'category',
+        triggerSelectOnValidInput: false,
+        tabDisabled: true,
+        autoSelectFirst: true,
         lookupFilter: function (suggestion, query, queryLowerCase) {
             return suggestion.value.toLowerCase().indexOf(queryLowerCase) === 0 || suggestion.value.toLowerCase().indexOf(queryLowerCase) !== -1 || suggestion.data.category.toLowerCase().indexOf(queryLowerCase) === 0 || suggestion.data.category.toLowerCase().indexOf(queryLowerCase) !== -1; //checking with both link label and category (both beginning and anywhere)
         }
     });
-    
-    $('#autocomplete-dynamic').on("change paste keyup input", function() {
-        if ($(this).val() == "") {
-            resetButton()
+
+    // Disable previous href when typing anything else, prevents jumping to link on enter
+    $('#autocomplete-dynamic').on("change paste input", function() {
+        resetButton();
+    });
+
+    // Go to site when hitting enter
+    $('#autocomplete-dynamic').keydown(function(event){
+    	if(event.keyCode == 13) {
+      	    event.preventDefault();
+
+            var url = $('#go-btn').attr('href');
+            if (url != "/") {
+                window.location.href = url;
+            }
+            return false;
         }
     });
-    
+
     //Disable link button after input is cleared
     $('#autocomplete-dynamic').closest('form').on('reset', function(event) {
         resetButton()
     });
-    
+
     //Disable link button by default
     resetButton()
 });
